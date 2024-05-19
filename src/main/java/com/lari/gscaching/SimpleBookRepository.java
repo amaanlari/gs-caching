a0aa035c528ceaaa09a0037c221df45668c8345d
amaanlari <amaanlari@outlook.comm>
2024-05-19 18:32:33 +0530

    enable caching with spring caching

--- src/main/java/com/lari/gscaching/SimpleBookRepository.java
+++ src/main/java/com/lari/gscaching/SimpleBookRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class SimpleBookRepository implements BookRepository {
 
     @Override
+    @Cacheable("books")
     public Book getByIsbn(String isbn) {
         simulateSlowService();
         return new Book(isbn, "Some book");
